896e888b72dfcde206fa247cfb6d2cae0c09a6a9
Thamska <165489803+HugoUddmar@users.noreply.github.com>
2026-02-18 16:31:09 +0100

    gjorde databas, slimfiler, app.rb, Read

--- app.rb
+++ app.rb
@@ -3,3 +3,27 @@ require 'slim'
 require 'sqlite3'
 require 'sinatra/reloader'
 require 'bcrypt'
+
+def db()
+  db = SQLite3::Database.new("db/databas.db")
+  return db
+end
+
+enable :sessions
+
+get('/') do
+  slim(:"planeshop/welcomepage")
+end
+
+get('/index') do
+  slim(:"planeshop/index")
+end
+
+get("/index/militaryplanes") do
+  db = db()
+  results_as_hash = true
+
+  @militaryplanes = db.execute("SELECT * FROM airplanes WHERE typeid = 1")
+
+  slim(:"planeshop/militaryplanes")
+end
--- db/seeder.rb
+++ db/seeder.rb
@@ -15,21 +15,35 @@ def seed!(db)
 end
 
 def drop_tables(db)
-  db.execute('DROP TABLE IF EXISTS exempel')
+  db.execute('DROP TABLE IF EXISTS airplanes')
+  db.execute('DROP TABLE IF EXISTS types')
+  db.execute('DROP TABLE IF EXISTS enginetypes')
 end
 
 def create_tables(db)
-  db.execute('CREATE TABLE exempel (
+  db.execute('CREATE TABLE airplanes (
               id INTEGER PRIMARY KEY AUTOINCREMENT,
               name TEXT NOT NULL, 
               description TEXT,
-              state BOOLEAN)')
+              state BOOLEAN,
+              price INTEGER,
+              topspeed INTEGER,
+              typeid INTEGER,
+              enginetypeid INTEGER)')
+
+  db.execute('CREATE TABLE types (
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              name TEXT NOT NULL)')
+
+  db.execute('CREATE TABLE enginetypes (
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              name TEXT NOT NULL)')
 end
 
 def populate_tables(db)
-  db.execute('INSERT INTO exempel (name, description, state) VALUES ("Köp mjölk", "3 liter mellanmjölk, eko",false)')
-  db.execute('INSERT INTO exempel (name, description, state) VALUES ("Köp julgran", "En rödgran",false)')
-  db.execute('INSERT INTO exempel (name, description, state) VALUES ("Pynta gran", "Glöm inte lamporna i granen och tomten",false)')
+  db.execute('INSERT INTO airplanes (name, description, state, price, topspeed, typeid, enginetypeid) VALUES ("Draken", "Swedish military plane by Saab very cool",false,5,2000,1,1)')
+  db.execute('INSERT INTO types (name) VALUES ("militaryplane")') 
+  db.execute('INSERT INTO enginetypes (name) VALUES ("jetafterburner")') 
 end
 
 
