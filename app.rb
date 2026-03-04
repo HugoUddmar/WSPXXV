2cd64588801b94174ae4fdd72e0e19e90ce4d1e7
Thamska <165489803+HugoUddmar@users.noreply.github.com>
2026-03-04 14:48:57 +0100

    Glömde ladda upp förra veckan har inte gjort något idag. Jag gjorde crud och en inner join

--- app.rb
+++ app.rb
@@ -21,9 +21,92 @@ end
 
 get("/index/militaryplanes") do
   db = db()
-  results_as_hash = true
+  db.results_as_hash = true
 
-  @militaryplanes = db.execute("SELECT * FROM airplanes WHERE typeid = 1")
+  @militaryplanes = db.execute("SELECT * FROM airplanes INNER JOIN enginetypes ON airplanes.enginetypeid = enginetypes.id WHERE typeid = ?",1)
 
   slim(:"planeshop/militaryplanes")
+end
+
+get("/index/airliners") do
+  db = db()
+  db.results_as_hash = true
+
+  @airliners = db.execute("SELECT * FROM airplanes INNER JOIN enginetypes ON airplanes.enginetypeid = enginetypes.id WHERE typeid = ?",2)
+
+  slim(:"planeshop/airliners")
+end
+
+get("/index/privatejets") do
+  db = db()
+  db.results_as_hash = true
+
+  @privatejets = db.execute("SELECT * FROM airplanes INNER JOIN enginetypes ON airplanes.enginetypeid = enginetypes.id WHERE typeid = ?",3)
+
+  slim(:"planeshop/privatejets")
+end
+
+get("/index/smallplanes") do
+  db = db()
+  db.results_as_hash = true
+
+  @smallplanes = db.execute("SELECT * FROM airplanes INNER JOIN enginetypes ON airplanes.enginetypeid = enginetypes.id WHERE typeid = ?",4)
+
+  slim(:"planeshop/smallplanes")
+end
+
+get("/planes/new") do
+
+  slim(:"planeshop/new")
+end
+
+post("/planes") do
+  db = db()
+  db.results_as_hash = true
+  name = params[:n]
+  description = params[:d]
+  price = params[:p]
+  topspeed = params[:ts]
+  type = params[:ty]
+  enginetype = params[:et]
+
+  db.execute("INSERT INTO airplanes (name, description, price, topspeed,typeid,enginetypeid) VALUES (?,?,?,?,?,?)",[name,description,price,topspeed,type,enginetype])
+  redirect('/index')
+end
+
+get('/planes/:id/edit') do
+  db = db()
+  db.results_as_hash = true
+  id = params[:id].to_i
+  @selected_plane = db.execute("SELECT * FROM airplanes WHERE id = ?",id).first
+
+  slim(:"planeshop/edit")
+end
+
+post('/planes/:id/update') do
+  db = db()
+
+  id = params[:id].to_i
+  name = params[:n]
+  description = params[:d]
+  price = params[:p]
+  topspeed = params[:ts]
+  type = params[:ty]
+  enginetype = params[:et]
+
+  db.execute('UPDATE airplanes SET name=?, description=?, price=?, topspeed=?, typeid=?, enginetypeid=? WHERE id=?', [name, description, price, topspeed, type, enginetype, id])
+
+  redirect('/index')
+end
+
+post('/planes/:id/delete') do
+  db = db()
+  denna_ska_bort = params[:id].to_i
+  db.execute("DELETE FROM airplanes WHERE id = ?",denna_ska_bort)
+  redirect('/index')
+end
+
+get('/user1planes') do
+  db = db()
+  @airplanes = db.execute("SELECT * FROM user_plane_rel INNER JOIN airplanes ON user_plane_rel.aid = airplanes.id WHERE uid = 2")
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -18,14 +18,16 @@ def drop_tables(db)
   db.execute('DROP TABLE IF EXISTS airplanes')
   db.execute('DROP TABLE IF EXISTS types')
   db.execute('DROP TABLE IF EXISTS enginetypes')
+  db.execute('DROP TABLE IF EXISTS users')
+  db.execute('DROP TABLE IF EXISTS user_plane_rel')
 end
 
 def create_tables(db)
   db.execute('CREATE TABLE airplanes (
               id INTEGER PRIMARY KEY AUTOINCREMENT,
-              name TEXT NOT NULL, 
+              name TEXT, 
               description TEXT,
-              state BOOLEAN,
+              status TEXT,
               price INTEGER,
               topspeed INTEGER,
               typeid INTEGER,
@@ -37,13 +39,33 @@ def create_tables(db)
 
   db.execute('CREATE TABLE enginetypes (
               id INTEGER PRIMARY KEY AUTOINCREMENT,
-              name TEXT NOT NULL)')
+              enginename TEXT NOT NULL)')
+  
+  db.execute('CREATE TABLE users(
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              name TEXT NOT NULL,
+              state TEXT,
+              pwddigest TEXT)')
+
+  db.execute('CREATE TABLE user_plane_rel(
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              uid INTEGER,
+              aid INTEGER)')
 end
 
 def populate_tables(db)
-  db.execute('INSERT INTO airplanes (name, description, state, price, topspeed, typeid, enginetypeid) VALUES ("Draken", "Swedish military plane by Saab very cool",false,5,2000,1,1)')
+  db.execute('INSERT INTO airplanes (name, description, status, price, topspeed, typeid, enginetypeid) VALUES ("Draken", "Swedish military plane by Saab very cool","in stock",5,2000,1,1)')
+  db.execute('INSERT INTO airplanes (name, description, status, price, topspeed, typeid, enginetypeid) VALUES ("Cessna 172", "Most popular small plane","in stock",0.2,500,4,2)')
   db.execute('INSERT INTO types (name) VALUES ("militaryplane")') 
-  db.execute('INSERT INTO enginetypes (name) VALUES ("jetafterburner")') 
+  db.execute('INSERT INTO types (name) VALUES ("airliner")')  
+  db.execute('INSERT INTO types (name) VALUES ("private jet")')
+  db.execute('INSERT INTO types (name) VALUES ("small plane")') 
+  db.execute('INSERT INTO enginetypes (enginename) VALUES ("jetafterburner")') 
+  db.execute('INSERT INTO enginetypes (enginename) VALUES ("propeller")') 
+  db.execute('INSERT INTO users (name,state,pwddigest) VALUES ("AdminKing","admin",1)')
+  db.execute('INSERT INTO users (name,state,pwddigest) VALUES ("Airplanebuyer67","user",123)')
+  db.execute('INSERT INTO user_plane_rel (uid,aid) VALUES (2,1)')
+  db.execute('INSERT INTO user_plane_rel (uid,aid) VALUES (2,2)')
 end
 
 
