bc9ccac3d1a070b9bb54354408f5e8e302eac871
Thamska <165489803+HugoUddmar@users.noreply.github.com>
2026-04-03 20:23:46 +0200

    Yardoc

--- app.rb
+++ app.rb
@@ -5,6 +5,12 @@ enable :sessions
 also_reload 'model'
 require_relative './model.rb'
 
+include Model
+
+#Returns the amount of commas ',' in a string
+# 
+# input [String] string, the string which the function will count the commas on
+#
 def countcommas(string)
   i = 0
   count = 0
@@ -17,6 +23,10 @@ def countcommas(string)
   return count
 end
 
+# Returns the 4 earliest times of the input string and the time now in a string and the difference between the time now and the earliest time
+# 
+# input [String] timeattempts, the string to be iterated
+#
 def iteratetimeattempts(timeattempts)
   if timeattempts == nil
     timeattempts = Time.now.to_i.to_s + ","
@@ -60,6 +70,8 @@ def iteratetimeattempts(timeattempts)
   return [timeattempts,first - last]
 end
 
+# Checks if the state of the user is a admin, otherwise it throwns an error
+#
 def admincheck()
   if session[:user_state] != "admin"
     session[:error] = "Du är inte admin"
@@ -73,10 +85,14 @@ before('/') do
   end
 end
 
+# Displays the Landing Page
+#
 get('/') do
   slim(:"planeshop/welcomepage")
 end
 
+# Displays buttons that if clicked show the Airplanes of a certain type, if logged in user also a button to show the user's bought planes, if admin also a button to create an Airplane
+#
 get('/planes') do
   @id = session[:user_id]
   @state = session[:user_state]
@@ -94,10 +110,17 @@ before('/planes/new') do
   admincheck()
 end
 
+# Displays a create Airplane form
+#
 get("/planes/new") do 
   slim(:"planeshop/new")
 end
 
+# Displays the Airplanes of a type, if logged in user also a buy button under every unsold Airplane, if admin also two buttons under every Airplane for edit and delete of that Airplane
+#
+# @param [Integer] :typeid, the ID of the type of the Airplane
+#
+# @see Model#planes
 get("/planes/:typeid") do
   typeid = params[:typeid]
 
@@ -118,6 +141,16 @@ before('/planes') do
   end
 end
 
+# Creates a new Airplane and redirects to '/planes'. Also validates the params.
+#
+# @param [String] name, The name of the Airplane
+# @param [String] description, The description of the Airplane
+# @param [String] price, The price of hte Airplane
+# @param [String] topspeed, The top speed of the Airplane
+# @param [Integer] typeid, ID of the type of the Airplane
+# @param [Integer] enginetypeid, ID of the enginetype of the Airplane
+#
+# @see Model#addplane
 post("/planes") do
   name = params[:n]
   description = params[:d]
@@ -149,6 +182,9 @@ before('/planes/*/*')do
   admincheck()
 end
 
+# Displays a edit Airplane form
+#
+# @see Model#selected_plane
 get('/planes/:id/edit') do
   id = params[:id].to_i
 
@@ -157,6 +193,17 @@ get('/planes/:id/edit') do
   slim(:"planeshop/edit")
 end
 
+# Updates an existing Airplane and redirects to '/planes'. Also validates the params
+#
+# @param [Integer] id, The id of the Airplane
+# @param [String] name, The name of the Airplane
+# @param [String] description, The description of the Airplane
+# @param [String] price, The price of hte Airplane
+# @param [String] topspeed, The top speed of the Airplane
+# @param [Integer] typeid, ID of the type of the Airplane
+# @param [Integer] enginetypeid, ID of the enginetype of the Airplane
+#
+# @see Model#update_plane
 post('/planes/:id/update') do
   db = db()
 
@@ -187,18 +234,34 @@ post('/planes/:id/update') do
   redirect('/planes')
 end
 
+# Deletes an existing Airplane and redirects to '/planes'
+#
+# @param [Integer] :id, The ID of the Airplane
+#
+# @see Model#delete
 post('/planes/:id/delete') do
   denna_ska_bort = params[:id].to_i
-  
+
   delete(denna_ska_bort)
 
   redirect('/planes')
 end
 
+# Displays a register form
+#
 get('/register') do
   slim(:"planeshop/register")
 end
 
+# Attempts registering an account and redirects to '/login'
+#
+# @param [String] name, The username
+# @param [String] password, The password
+# @param [String] confirmpassword, The repeated password
+#
+# @see Model#adduser
+# @see Model#idresult
+# @see Model#pwd_digest
 post('/register') do
   name = params[:q]
   password = params[:a]
@@ -235,10 +298,20 @@ post('/register') do
   end
 end
 
+# Displays a login form
+#
 get('/login') do
   slim(:"planeshop/login")
 end
 
+# Attempts login, updates the session and redirects to 'planes'
+#
+# @param [String] name, The username
+# @param [String] password, The password
+#
+# @see Model#userresult
+# @see Model#updatetimeattempts
+# @see Model#pwd_digest2
 post('/login') do
   name = params[:q]
   password = params[:a]
@@ -282,10 +355,14 @@ post('/login') do
   end
 end
 
+# Displays the logged in user's bought planes
+#
+# @param [Integer] :idcheck, The ID of the User
+#
+# @see Model#myplanes
 get('/myplanes/:id') do
   id = session[:user_id]
   idcheck = params[:id].to_i
-  p "#{reltable()}"
   if idcheck == id
     @airplanes = myplanes(id)
     slim(:"planeshop/myplanes")
@@ -302,18 +379,28 @@ before('/addplanetouser/:planeid') do
   end
 end
 
+# Adds a plane to a user in the user_plane_rel table and redirects to '/planes'
+#
+# @param [Integer] :uid, The ID of the User
+# @param [Integer] :aid, The ID of the Airplane
+#
+# @see Model#addplanetouser
 post('/addplanetouser/:planeid') do
   uid = session[:user_id]
   aid = params[:planeid].to_i
-  addplanestouser(uid,aid)
+  addplanetouser(uid,aid)
   redirect('/planes')
 end
 
+# Clears the Cache/Resets the session
+#
 post('/clear') do
   session.clear
   redirect('/')
 end
 
+# Displays an error message
+#
 get('/error') do
   slim(:"planeshop/error")
 end
